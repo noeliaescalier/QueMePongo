40fd8d30843a4aeddd3148dd9c2540eb393715d7
noeliaescalier <noesc@frba.utn.edu.ar>
2024-06-20 23:07:52 -0300

    Se completa con los atributos en la PrendaBorrador y se agregan las validaciones en esa clase
    se saca las validaciones de la prenda

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -7,25 +7,14 @@ public class Prenda {
 
     private Color colorSecundario;
 
-    private String marca;
+    private Trama trama;
 
-    public Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal,Color colorSecundario) {
-
-        if(tipoPrenda == null){
-            throw new IllegalArgumentException("El tipo de prenda es obligatorio.");
-        }
-
-        if(material == null){
-            throw new IllegalArgumentException("El material es obligatorio.");
-        }
-
-        if(colorPrincipal == null){
-            throw new IllegalArgumentException("El color principal es obligatorio.");
-
-        }
-
-
+    public Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal, Color colorSecundario,Trama trama) {
+        this.tipoPrenda = tipoPrenda;
+        this.material = material;
+        this.colorPrincipal = colorPrincipal;
+        this.colorSecundario = colorSecundario;
+        this.trama = trama;
     }
-
 }
 
--- src/main/java/PrendaBorrador.java
+++ src/main/java/PrendaBorrador.java
@@ -1,3 +1,53 @@
 public class PrendaBorrador {
-    Prenda prenda;
+    private TipoPrenda tipoPrenda;
+    private Material material;
+
+    private Trama trama = Trama.LISA;
+
+    private Color colorPrincipal;
+
+    private Color colorSecundario;
+
+
+    public void especificarTipo(TipoPrenda tipoPrenda){
+        if(tipoPrenda == null){
+            throw new IllegalArgumentException("El tipo de prenda no puede ser nulo.");
+        }
+    }
+
+    public void especificarMaterial(Material material) {
+        if(material == null){
+            throw new IllegalArgumentException("El material no puede ser nulo.");
+        }
+    }
+
+    public void especificarColorPrincipal(Color colorPrincipal){
+        if(colorPrincipal == null){
+            throw new IllegalArgumentException("El material no puede ser nulo.");
+        }
+    }
+
+    public void especificarTrama(Trama trama) {
+        if(trama == null) {
+            trama =Trama.LISA;
+        }else{
+            this.trama = trama;
+        }
+    }
+
+    public Prenda crearPrenda() {
+        if(tipoPrenda == null){
+            throw new IllegalArgumentException("El tipo de prenda es obligatorio.");
+        }
+
+        if(material == null){
+            throw new IllegalArgumentException("El material es obligatorio.");
+        }
+
+        if(colorPrincipal == null){
+            throw new IllegalArgumentException("El color principal es obligatorio.");
+
+        }
+        return new Prenda(tipoPrenda,material,colorPrincipal,colorSecundario,trama);
+    }
 }
