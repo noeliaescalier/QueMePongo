bbe2e31bfd34f6f1f3754dc5ccdb434489b4cd97
noeliaescalier <noesc@frba.utn.edu.ar>
2024-06-23 15:02:18 -0300

    modifico ServicioMeteorologicoAccuWeather.java
    ahora em devuelve la temperatura.

--- src/main/java/ServicioMeteorologico.java
+++ src/main/java/ServicioMeteorologico.java
@@ -1,4 +1,4 @@
 public interface ServicioMeteorologico {
 
-  
+  public Integer obtenerCondicionesClimaticas();
 }
--- src/main/java/ServicioMeteorologicoAccuWeather.java
+++ src/main/java/ServicioMeteorologicoAccuWeather.java
@@ -9,6 +9,8 @@ public class ServicioMeteorologicoAccuWeather implements ServicioMeteorologico {
   private final String direccion;
 
   private LocalDateTime proximaExpiracion;
+ //lo llamo condicionClimatica por si en el futuro tenga que guardar ademas de la temperatura algun otro dato
+  private Integer condicionClimatica;
 
 
   public ServicioMeteorologicoAccuWeather(AccuWeatherAPI api, Duration periodoValidez, String direccion) {
@@ -17,22 +19,28 @@ public class ServicioMeteorologicoAccuWeather implements ServicioMeteorologico {
     this.direccion = direccion;
   }
 
-    //TODO;
-  /*
-    public Map<String,Object> obtenerCondicionesClimaticas() {
+    public Integer obtenerCondicionesClimaticas() {
       if(this.expiro()){
+        condicionClimatica = consultarApiTemperatura(); ;
         proximaExpiracion = LocalDateTime.now().plus(this.periodoValidez);
 
       }
-
+      return this.condicionClimatica;
     }
-*/
-  private LocalDateTime proximaExpiracion() {
-    return LocalDateTime.now().plus(this.periodoValidez);
-  }
+
 
   private Boolean expiro(){
-    return proximaExpiracion().isAfter(LocalDateTime.now());
+    return proximaExpiracion.isAfter(LocalDateTime.now());
+  }
+
+  public Integer consultarApiTemperatura(){
+    Map<String, Object> respuestaClima = this.api.getWeather(this.direccion).get(0);
+    Map<String, Object> temperatura = (Map<String, Object>) respuestaClima.get("Temperature");
+    Integer valorDeTemperatura = (Integer) temperatura.get("Value");
+    String unidadDeTemperatura = (String) temperatura.get("Unit");
+    //TODO VER EL CALCULO CUANDO LA TEMPERATURA NO VENGA EN CELCIUS
+    return  valorDeTemperatura;
+
   }
 
 }
